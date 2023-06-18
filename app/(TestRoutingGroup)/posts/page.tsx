28e6bab92f88f91a5b8151262d4ed51b122aeef3
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2023-06-18 15:37:53 +0500

    Create nextjs-app

---
+++ app/(TestRoutingGroup)/posts/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import Link from 'next/link'
+// import { useRouter } from 'next/navigation'
+
+export default function Posts() {
+    // const router = useRouter();
+    return (
+        <main className={styles.main}>
+            <div className={styles.center}>
+                <h1>Post</h1>
+            </div>
+            <Link href="/">Back to Home</Link>
+            {/* <button type="button" onClick={() => router.push("/")}>Back to Home</button> */}
+        </main>
+    )
+}
